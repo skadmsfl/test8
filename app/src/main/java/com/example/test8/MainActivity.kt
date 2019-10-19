5834fdd4d4f28a3702533b7441f35cce49921125
skadmsfl <noeytt.pk@gmail.com>
2019-10-19 23:57:21 +0700

    Log

--- app/src/main/java/com/example/test8/MainActivity.kt
+++ app/src/main/java/com/example/test8/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.test8
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
